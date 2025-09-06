94d16a01b8383c9b7da1930632f5ccb613f4f610
Flora Denoulet <flora.denoulet@student.howest.be>
2025-09-06 22:03:48 +0200

    Add transparancy to body

--- assets/js/handlers.js
+++ assets/js/handlers.js
@@ -8,9 +8,7 @@ function tryPassword(e) {
 function checkPassword(guessedPassword) {
     if (pwdIsCorrect(encrypt(guessedPassword))) {
         console.log("Correct");
-        document.querySelector("body").classList.add("pink");
     } else {
         console.log("Incorrect");
-        document.querySelector("body").classList.remove("pink");
     }
 }
